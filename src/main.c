619590bbc1a299353e66d0ef59d0a35e5129b721
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-02-27 19:00:08 +0530

    Visualize sound using linear scale

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <raylib.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include "kiss_fft.h"
 
 // --- CONSTANTS ------------>
 
@@ -8,6 +10,12 @@
 #define HEIGHT 600
 
 #define NSAMPLES 1024
+
+#define NBARS 32
+#define BAR_WIDTH 20
+
+#define NGROUPS (((NSAMPLES) / 2) / NBARS)
+#define MAG_SCALE 2.0f
 
 // --- GLOBALS ------------>
 
@@ -17,6 +25,7 @@ int COUNT = 0;
 // --- PROTOTYPES ------------>
 
 void Callback(void *, unsigned int);
+void DrawBars(const float *);
 
 // --- ENTRY POINT ------------>
 
@@ -41,16 +50,41 @@ int main(void)
 		CloseWindow();
 		exit(EXIT_FAILURE);
 	}
-	SetAudioStreamCallback(music.stream, Callback);
+	AttachAudioStreamProcessor(music.stream, Callback);
 	PlayMusicStream(music);
+
+	kiss_fft_cfg cfg = kiss_fft_alloc(NSAMPLES, 0, NULL, NULL);
+
+	kiss_fft_cpx fin[NSAMPLES];
+	kiss_fft_cpx fout[NSAMPLES];
+	float mag[NSAMPLES / 2];
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
+
 		ClearBackground(BLACK);
 		UpdateMusicStream(music);
+
+		if (COUNT == 0)
+		{
+			for (int i = 0; i < NSAMPLES; i++)
+			{
+				fin[i].r = BUFFER[i];
+				fin[i].i = 0.0f;
+			}
+			kiss_fft(cfg, fin, fout);
+		}
+
+		for (int j = 0; j < NSAMPLES / 2; j++)
+			mag[j] = hypotf(fout[j].i, fout[j].r);
+
+		DrawBars(mag);
+
 		EndDrawing();
 	}
+
+	kiss_fft_free(cfg);
 
 	CloseAudioDevice();
 	CloseWindow();
@@ -62,12 +96,35 @@ int main(void)
 
 void Callback(void *buffer, unsigned int frames)
 {
-	unsigned int copy = frames;
-	if (COUNT + frames > NSAMPLES)
-		copy = NSAMPLES - COUNT;
-	memcpy(&BUFFER[COUNT], (float *)buffer, copy * sizeof(float));
-	COUNT += copy;
+	float *samples = (float *)buffer;
+	for (unsigned int i = 0; i < frames && COUNT < NSAMPLES; i++)
+		BUFFER[COUNT++] = samples[i * 2]; // left channel only
 	if (COUNT >= NSAMPLES)
 		COUNT = 0;
 	return;
+}
+
+void DrawBars(const float *magnitudes)
+{
+	float groups[NBARS];
+	int nmax = 0;
+	float max = -1;
+	for (int i = 0; i < NSAMPLES / 2; i++)
+	{
+		if (magnitudes[i] > max)
+			max = magnitudes[i];
+
+		if (i % NGROUPS == NGROUPS - 1)
+		{
+			groups[nmax++] = max;
+			max = -1;
+		}
+	}
+
+	int height;
+	for (int j = 0; j < NBARS; j++)
+	{
+		height = groups[j] * MAG_SCALE;
+		DrawRectangle(j * (WIDTH / NBARS), HEIGHT - height, BAR_WIDTH, height, RAYWHITE);
+	}
 }
