4ea441efc8e89490eefd9a7fb1f674058bbf1b25
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-02-27 16:14:03 +0530

    Register callback

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
 #include <raylib.h>
 #include <stdlib.h>
+#include <string.h>
 
 // --- CONSTANTS ------------>
 
 #define WIDTH 900
 #define HEIGHT 600
 
+#define NSAMPLES 1024
+
+// --- GLOBALS ------------>
+
+float BUFFER[NSAMPLES];
+int COUNT = 0;
+
 // --- PROTOTYPES ------------>
+
+void Callback(void *, unsigned int);
 
 // --- ENTRY POINT ------------>
 
@@ -31,6 +41,7 @@ int main(void)
 		CloseWindow();
 		exit(EXIT_FAILURE);
 	}
+	SetAudioStreamCallback(music.stream, Callback);
 	PlayMusicStream(music);
 
 	while (!WindowShouldClose())
@@ -48,3 +59,15 @@ int main(void)
 }
 
 // --- IMPLEMENTATIONS ------------>
+
+void Callback(void *buffer, unsigned int frames)
+{
+	unsigned int copy = frames;
+	if (COUNT + frames > NSAMPLES)
+		copy = NSAMPLES - COUNT;
+	memcpy(&BUFFER[COUNT], (float *)buffer, copy * sizeof(float));
+	COUNT += copy;
+	if (COUNT >= NSAMPLES)
+		COUNT = 0;
+	return;
+}
